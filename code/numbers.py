1470b3928ac8e79fd608cc4c58355a9d7274d9b4
jttafuri@syr.edu <jttafuri@syr.edu>
2024-09-05 21:54:36 -0400

    Completed assignment

--- code/numbers.py
+++ code/numbers.py
@@ -11,4 +11,17 @@ For example, if the input is:
 The output should be:
 {'odd': [3, 5], 'even': [2, 4, 6]}
 '''
+even_values = []  # Initialize as an empty list
+odd_values = []  # Initialize as an empty list
+inputted_number = 1 # Initialize as a int value
 
+while inputted_number != 0:
+    inputted_number = int(input("Enter a number: "))
+    if inputted_number == 0:
+        break
+    if inputted_number % 2 == 0:  #checks for even values
+        even_values.append(inputted_number)  # Append the number to the even list
+    else:
+        odd_values.append(inputted_number)  # Append the number to the odd list
+dictionary = {'odd': odd_values, 'even': even_values}
+print(dictionary)
